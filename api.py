16f99cd22259de363a34c083e7395d2ebbcd0e2d
Rajab Mammadov <76522189+rajab11@users.noreply.github.com>
2024-12-20 22:17:22 +0100

    getting JSON data using python requests, reading API_KEY from file

--- api.py
+++ api.py
@@ -1,2 +1,21 @@
+from dotenv import load_dotenv
+import os
 import requests
+import json
+
+load_dotenv()
+
+API_URL="https://api.openweathermap.org/data/2.5/weather?"
+CITY="London"
+API_KEY=os.getenv("API_KEY")
+
+params={"q":CITY,"appid":API_KEY}
+response=requests.get(API_URL,params=params)
+
+if response.status_code==200:
+    weather_data=response.json()
+    print("Fetched data:",json.dumps(weather_data,indent=4))
+else:
+    print("Failed to get data.Status code: ",response.status_code)
+
 
